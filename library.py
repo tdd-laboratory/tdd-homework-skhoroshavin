778f5b67a1ffb22cd059058b2bc392f8c9eaa0b7
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2018-06-21 17:02:48 +0300

    Step 10: Refactor regex for date fmt2
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- library.py
+++ library.py
@@ -4,7 +4,7 @@ _whole_word = lambda x: re.compile(r'(?<=\W)' + x + '(?=\W)')
 _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
 _integer_pat = _whole_word(r'\d+')
 _date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
-_date_fmt2_pat = _whole_word(r'\d{2} (J[au]n|Feb|Ma[ry]|Apr|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
+_date_fmt2_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
 
